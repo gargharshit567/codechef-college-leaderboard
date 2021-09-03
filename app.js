457975d01c7fcea91e516c5fe877b3770a85955b
gargharshit567 <64560339+gargharshit567@users.noreply.github.com>
2021-09-03 12:31:06 +0530

    Add files via upload
    problem of L_SCORE

--- app.js
+++ app.js
@@ -1,20 +1,214 @@
-app.post("/", function(req,res){
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const app = express();
+var axios = require('axios').default;
+const mongoose = require("mongoose");
+var users = [];
+const challenge = 1;
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/leaderDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+const memberSchema = new mongoose.Schema({
+  username: String, // codechef username
+  L_Score: Number, //leaderboard score
+  L_rank: Number, // leaderboard rank
+  division: Number,
+  rating:Number,
+  problem_solved: Number,
+  //old_rank: Number,
+  old_score:Number,
+  old_problems: Number
+});
+const Members = mongoose.model("Member", memberSchema);
+var total_st; //new
+var sorted_members=[];
+var options = {
+  method: 'POST',
+  url: 'https://api.codechef.com/oauth/token',
+  headers: {
+    'content-type': 'application/json'
+  },
+  data: {
+    grant_type: 'client_credentials',
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    scope: 'public',
+    redirect_uri: 'http://127.0.0.1:3000/result'
+  }
+};
+app.post("/", function(req, res) {
   const contestCode = req.body.contestCode;
-  const offset = req.body.offset;
-  const cn= req.body.contestNumber;
-  const url="https://api.codechef.com/rankings/"+contestCode+"?fields=username,totalScore&institution=Mugneeram%20Bangur%20Memorial%20Engineering%20College%2C%20Jodhpur&offset="+offset+"&sortBy=rank";"
+  const div = req.body.division;
+  //console.log(contestCode);
+  const cn = req.body.contestNumber;
+  const url = "https://api.codechef.com/rankings/" + contestCode + "?fields=username,totalScore,rating,problemScore&institution=Mugneeram%20Bangur%20Memorial%20Engineering%20College%2C%20Jodhpur";
+  //  'https://api.codechef.com/rankings/LTIME99C?fields=username,totalScore&institution=Mugneeram%20Bangur%20Memorial%20Engineering%20College%2C%20Jodhpur',
+  const weightage = [[15,30,30,25],[25,20,20,35]];
+
+  axios.request(options).then(function(response) {
+    console.log("coming in axios 1st....");
+    //console.log(response.data.result.data.access_token);
+    access_token = response.data.result.data.access_token;
+    console.log("access_token :"+access_token);
+    axios.request({
+      method: 'GET',
+      url: url,
+      headers: {
+        'content-type': 'application/json',
+        authorization: 'Bearer ' + access_token
+      }
+    }).then(function(result) {
+      //console.log(result.data.result.data.content);
+      users = result.data.result.data.content;
+      //console.log("users : ");
+      //console.log(users);
+      var cnt=1;
+      var rank= users[0].rank;
+      var top_score= parseFloat(users[0].totalScore);
+       total_st= users.length; //new
+      users.forEach(function(user){
+         if(user.rank == rank)user.rank= cnt;
+         else
+         {
+           cnt++;
+           rank= user.rank;
+           user.rank=cnt;
+         }
+         user['problem_solved']= Math.floor(parseInt(user.totalScore/100));
+         user.totalScore = parseFloat(((parseFloat(user.totalScore )* weightage[div-2][cn-1])/(top_score* Math.pow(user.rank,(1.0 / total_st)))*100.0).toFixed(2));
+        if(user.username==="piyush_482000")console.log(user);
+        if(user.username==="harsshar14")console.log(user);
+      });
+
+
+     var debug=0;
+      users.forEach(function(user) {
+        debug++;
+        Members.findOne({
+          username: user.username
+        }, function(err, resu) {
+          if (err)
+            console.log(err);
+          else {
+            if (resu) {
+              //console.log(parseInt(user.totalScore));
+              if(cn!=1)
+              {
+               resu.old_score= resu.L_Score;
+               resu.old_problems= resu.problem_solved;
+               resu.L_Score += user.totalScore;
+               resu.problem_solved += user.problem_solved;
+             }
+             else
+             {
+               resu.old_score= 0;
+               resu.old_problems= 0;
+               resu.L_Score= user.totalScore;
+               resu.problem_solved = user.problem_solved;
+             }
+              resu.L_rank = 0;
+              resu.division = div;
+              resu.rating = user.rating;
+
+              resu.save(function(err) {});
+              //console.log(resu);
+
+            } else {
+
+                const newItem = new Members({
+                username : user.username,
+                L_Score : user.totalScore,
+                old_score : 0,
+                old_problems : 0,
+                L_rank : 0,
+                rating : user.rating,
+                division : div,
+                problem_solved :  user.problem_solved
+
+              });
+              newItem.save();
+
+            }
+          }
+        });
+      });
+        console.log("debug :" +debug);
+    }).catch(function(error) {
+      console.error(error);
+    });
+  }).catch(function(error) {
+    console.error(error);
+
+  });
+  console.log("coming in main....")
+
+  res.redirect("/");
+});
+
+app.get("/admin_only", function(req, res) {
+  res.render("admin_only");
+});
+
+
+app.get("/", function(req, res) {
+//console.log(req.query);
+//console.log(Object.keys(req.query).length);
+//console.log(sorted_members);
+Members.find({division:3}).sort({L_Score:-1}).exec (function(err, found) {
+  // found = found.toArray();
+//  console.log(found);
+  found.forEach(function(item){
+    sorted_members.push(item);
+  });
+  var found1=sorted_members;
+  //console.log(sorted_members);
+  var count=1;
+  if (err)
+    console.log(err);
+  else {
+
+    var last= found1[0].L_Score;
+    for(var i=1; i<=found1.length ;i++) {
+      //const item= found[i-1];
+
+       if (last!==found1[i-1].L_Score) {
+         count=count+1;
+        last= found1[i-1].L_Score;
+       }
+    found1[i-1].L_rank=count;
+
+  }
+ sorted_members=found1;
+}
+});
+console.log("home route...");
+  var limit= 20,page=1;
+  var queries= req.query;
+  if(queries.hasOwnProperty("limit")){
+    limit= parseInt(queries.limit);
+  }
+  if(queries.hasOwnProperty("page"))page= parseInt(queries.page);
+  const tmp_arr= sorted_members.slice((page-1)*limit,page*limit);
+  const pages= Math.ceil(sorted_members.length/limit);
+  const paras= {
+    pages:pages,
+    limit:limit,
+    page:page
+  };
+  res.render("index", {data: tmp_arr, paras:paras}); // new
 
 });
 
-app.get("/login", function(req,res){
-  res.render("login");
-});
-app.post("/login",function(req,res){
-  if(req.body.admin_name === "CodeChefMBM2021" && req.body.admin_password === process.env.SECRET)
-  {
-    res.render("admin_only");
-  }
-  else{
-    res.redirect("/login")
-  }
+app.listen(3000, function(req, res) {
+  console.log("server started at port 3000");
 });
